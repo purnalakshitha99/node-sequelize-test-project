f5ae70e0eb7f2af672a50c79d9f670091569ad07
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-08-07 21:43:40 +0530

    user route to add auth related things

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
+const authController = require("../controllers/authController.js");
+const authenticate = require("../middleware/auth");
 
-router.post('/users', async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
+router.post("/register", authController.register);
+router.post("/login", authController.login);
 
 module.exports = router;
