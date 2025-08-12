6af8bcf4c3b00c9ec063f405ed44a4fcea48532b
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-08-12 21:44:48 +0530

    get all users function added

--- controllers/authController.js
+++ controllers/authController.js
@@ -55,3 +55,13 @@ exports.login = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
