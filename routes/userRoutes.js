542c4a66326fec1fae95406098cee1899bdc1868
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-08-12 21:45:26 +0530

    role base controll added to auth route to

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController.js");
-const authenticate = require("../middleware/auth");
+const authenticate = require("../middleware/auth.js");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+router.get("/users-all", authenticate, authController.getAllUsers);
 
 module.exports = router;
