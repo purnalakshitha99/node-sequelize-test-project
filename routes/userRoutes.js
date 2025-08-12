a26a971675e44e090e35bc5bd94fe98ae25f7618
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-08-12 22:14:39 +0530

    delete user by id success

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,5 +6,6 @@ const authenticate = require("../middleware/auth.js");
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/users-all", authenticate, authController.getAllUsers);
+router.delete("/users/:id", authenticate, authController.deleteUserById);
 
 module.exports = router;
